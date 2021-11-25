ce19c48591c8fbb46b2d4f70d122d53f7e8f9846
shivkumar98 <shiv_kumar.98@outlook.com>
2021-11-25 12:13:02 +0000

    first commit

---
+++ Vidley/Controllers/MovieController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidley.Models;
+
+namespace Vidley.Controllers
+{
+    public class MovieController : Controller
+    {
+
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Get: Random Movie
+        public ActionResult Random()
+        {
+
+            Movie movie = new Movie() { Name = "Shrek" };
+            return View(movie);
+        }
+    }
+}
